e1da3e8400f0e7b9acf68ebb0e7de455f08573f9
zett42 <18427509+zett42@users.noreply.github.com>
2026-01-26 20:43:40 +0100

    Add pause menu functionality and UI elements

---
+++ ui/pause_menu.gd
@@ -0,0 +1,53 @@
+extends CanvasLayer
+
+@onready var color_rect = $ColorRect
+
+func _ready():
+	# Make sure the pause menu is hidden initially
+	color_rect.modulate.a = 0
+	hide()
+
+func _input(event):
+	# Handle ESC key to toggle pause
+	if event.is_action_pressed("ui_cancel"):  # ESC key is mapped to ui_cancel by default
+		toggle_pause()
+
+func toggle_pause():
+	if visible:
+		fade_out_and_resume()
+	else:
+		pause_game()
+
+func pause_game():
+	get_tree().paused = true
+	show()
+	# Fade in
+	var tween = create_tween()
+	tween.tween_property(color_rect, "modulate:a", 1.0, 0.2)
+
+func fade_out_and_resume():
+	# Fade out
+	var tween = create_tween()
+	tween.tween_property(color_rect, "modulate:a", 0.0, 0.2)
+	await tween.finished
+	get_tree().paused = false
+	hide()
+
+func resume_without_fade():
+	get_tree().paused = false
+	color_rect.modulate.a = 0
+	hide()
+
+func _on_continue_button_pressed():
+	# Resume with fade effect
+	fade_out_and_resume()
+
+func _on_main_menu_button_pressed():
+	resume_without_fade()
+	# Return to start screen
+	get_tree().change_scene_to_file("res://ui/start.tscn")
+
+func _on_exit_button_pressed():
+	resume_without_fade()
+	# Quit the game
+	get_tree().quit()
