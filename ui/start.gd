209cca9aeb25ffec4b8552e0ee607538a8aa0f61
zett42 <18427509+zett42@users.noreply.github.com>
2026-01-26 20:31:03 +0100

    Add Exit button and functionality to the start scene

--- ui/start.gd
+++ ui/start.gd
@@ -19,5 +19,9 @@ func _on_hard_button_pressed():
 	get_tree().change_scene_to_file("res://ui/room.tscn")
 
 
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+
 func _on_about_button_pressed():
 	$About.show_about()
